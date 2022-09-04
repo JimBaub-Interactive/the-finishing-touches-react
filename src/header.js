222afb4e5ffcb78c8d6e8267c4dab063f783cc62
Jamie Neill <jamie@jimbaub.tech>
2022-09-04 18:09:04 +0100

    Header added

---
+++ src/header.js
@@ -0,0 +1,24 @@
+function Header() {
+  return (
+    <nav>
+      <div class="container nav-wrapper">
+        <a href="#" class="brand-logo left">
+          The Finishing Touches
+        </a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+          <li>
+            <a href="sass.html">Sass</a>
+          </li>
+          <li>
+            <a href="badges.html">Components</a>
+          </li>
+          <li>
+            <a href="collapsible.html">JavaScript</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default Header;
